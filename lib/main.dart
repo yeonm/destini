d45d62397984ae41078dd1478dea7a56ddb7cdce
yeonm <hoeyeon06@gmail.com>
2022-03-19 15:32:41 +0900

    First

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:destini_challenge_starting/story.dart';
+import 'package:destini_challenge_starting/story_brain.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+StoryBrain storyBrain = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+MaterialStateProperty<Color> getColor(Color color) {
+  return MaterialStateProperty.all(color);
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    storyBrain.nextStory(1);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: getColor(Colors.red),
+                  ),
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: TextButton(
+                  onPressed: () {
+                    //Choice 2 made by user.
+                    storyBrain.nextStory(2);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: getColor(Colors.blue),
+                  ),
+                  child: Text(
+                    storyBrain.getChoice2(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+
+//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
---
+++ lib/story.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
